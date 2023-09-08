9b4ed71463d121d1fae98afcaf89beb7e9d09f90
iMaeGoo <mail1st@qq.com>
2023-09-08 10:47:10 +0800

    feat: create netlify function twikoo

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -20,6 +20,6 @@ exports.handler = async function (event, context) {
   // }
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", event, context }),
   };
 };
